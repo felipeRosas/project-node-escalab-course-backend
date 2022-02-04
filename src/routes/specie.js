951a96b47d107911ec6515ecd1e5335e5630b931
felipeRosas <feliperozam@gmail.com>
2022-02-04 11:15:13 -0300

    documentación de especies completada

--- src/routes/specie.js
+++ src/routes/specie.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const { create, update, read, listAll, remove} = require('../controllers/specie')
+const { create, update, read, listAll, getAll, remove} = require('../controllers/specie')
 
 
 /**
@@ -60,6 +60,27 @@ router.put('/species/:slug', update)
 
 /**
  * @swagger
+ * /species:
+ *   get:
+ *     tags: [Specie]
+ *     summary: "get all species"
+ *     responses:
+ *       200:
+ *         description: return a list of species
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: "#/components/schemas/Specie"
+ *       400:
+ *         description: bad request, count is required
+ * 
+ */
+ router.get('/species', getAll)
+
+/**
+ * @swagger
  * /species/{count}:
  *   get:
  *     tags: [Specie]
@@ -71,22 +92,63 @@ router.put('/species/:slug', update)
  *         required: true
  *         type: "integer"
  *         format: "int64"
- *   responses:
- *     200:
- *       description: return a list of species
- *       content:
- *         application/json:
- *           schema:
- *             type: array
- *             items:
- *               $ref: "#/components/schemas/Specie"
+ *     responses:
+ *       200:
+ *         description: return a list of species
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: "#/components/schemas/Specie"
+ *       400:
+ *         description: bad request, count is required
  * 
  */
  router.get('/species/:count', listAll)
 
+ /**
+ * @swagger
+ * /species/{slug}:
+ *   get:
+ *     tags: [Specie]
+ *     summary: "get an specific specie"
+ *     description: "return a single specie"
+ *     parameters:
+ *       - name: "slug"
+ *         in: "path"
+ *         description: "slug of specie"
+ *         required: true
+ *         type: "string"
+ *     responses:
+ *       200:
+ *         description: return an specific specie
+ *         schema:
+ *           $ref: "#/components/schemas/Specie"
+ * 
+ */
 router.get('/species/:slug', read)
 
-
+ /**
+ * @swagger
+ * /species/{slug}:
+ *   delete:
+ *     tags: [Specie]
+ *     summary: "delete an specific specie"
+ *     description: "removes a species and returns this"
+ *     parameters:
+ *       - name: "slug"
+ *         in: "path"
+ *         description: "slug of specie"
+ *         required: true
+ *         type: "string"
+ *     responses:
+ *       200:
+ *         description: returns the deleted species
+ *         schema:
+ *           $ref: "#/components/schemas/Specie"
+ * 
+ */
 router.delete('/species/:slug', remove)
 
 
